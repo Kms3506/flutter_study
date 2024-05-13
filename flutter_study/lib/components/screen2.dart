5e79c874af15dc6ccf7046a42cda7ea6188c1257
Kms3506 <ixt@naver.com>
2024-05-13 19:54:24 +0900

    ListClick
    목록을 클릭했을 때 새로운 창을 띄움

--- flutter_study/lib/components/screen2.dart
+++ flutter_study/lib/components/screen2.dart
@@ -7,34 +7,60 @@ class Screen2 extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           _buildListItem(
+            context,
             image: 'flutter.png',
             title: 'Item 1',
-            subtitle: 'Description for Item 4',
+            subtitle: 'Test subtitle',
           ),
           _buildListItem(
+            context,
             image: 'flutter.png',
             title: 'Item 2',
-            subtitle: 'Description for Item 5',
+            subtitle: 'Test subtitle',
           ),
           _buildListItem(
+            context,
             image: 'flutter.png',
             title: 'Item 3',
-            subtitle: 'Description for Item 6',
+            subtitle: 'Test subtitle',
           ),
           // 추가적인 리스트 아이템들을 필요에 따라 추가할 수 있습니다.
         ],
       ),
     );
   }
-
-  Widget _buildListItem({required String image, required String title, required String subtitle}) {
+ Widget _buildListItem(BuildContext context, {required String image, required String title, required String subtitle}) {
     return ListTile(
       leading: Image.asset(image),
       title: Text(title),
       subtitle: Text(subtitle),
       onTap: () {
-        // 항목을 탭할 때 수행할 작업을 여기에 추가하세요.
+        // 해당 목록을 클릭하면 새로운 화면으로 이동합니다.
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailScreen(title: title),
+          ),
+        );
       },
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  final String title;
+
+  const DetailScreen({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text('Detail Screen for $title'),
+      ),
     );
   }
 }
